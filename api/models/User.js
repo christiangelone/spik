f84493a59f13c182405157d13478e37556384ea2
Christian Angelone <christiangelone@gmail.com>
2017-08-06 13:14:47 -0300

    Initializes project

---
+++ api/models/User.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const Model = require('trails/model')
+
+/**
+ * @module User
+ * @description User Model
+ */
+module.exports = class User extends Model {
+
+  static config () {
+
+  }
+
+  static schema () {
+
+  }
+}
